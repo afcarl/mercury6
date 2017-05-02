3e50eeee7c2f7dc0ce5ec5938a2d1f4d9af2f4c3
silburt <silburt@astro.utoronto.ca>
2017-05-02 14:36:52 -0400

    energy runs

--- calc_energy.py
+++ calc_energy.py
@@ -1,24 +1,29 @@
-#A.S. This just reads in the mercury6 calculated energies and plots it.
+#A.S. This just reads in a collection of mercury6 runs, calculates energies and plots it.
 
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import glob
 
 dir = sys.argv[1]
+files = glob.glob('%s/*/*eo.txt'%dir)
+
+colors = ['black','brown','red','chocolate','orange','gold','green','dodgerblue','blue','purple']
 
 #energy offset
-data=np.genfromtxt(dir+'eo.txt',delimiter=None, dtype=float)
-nr, nc = data.shape
-dE = np.zeros(nr)
-time = np.zeros(nr)
-for i in xrange(0,nr):
-    dE[i] = abs(data[i][1])
-    time[i] = data[i][0]*0.01721420632  #constant is 2pi/365
+for i,f in enumerate(files):
+    data = np.genfromtxt(f, delimiter=None, dtype=None, skip_header=2, skip_footer=2)
+    junk, time, junk, junk, dE, junk, dL, Np = zip(*data.T)
+    HSF = f.split('HSF')[1].split('_')[0]
+    plt.plot(time, dE, '.', color=colors[i], label='HSF=%s'%HSF)
 
+plt.legend(loc='lower right', fontsize=10)
 
 plt.plot(time,dE,'o')
+plt.plot(time,1e-7*np.sqrt(time))
 plt.yscale('log')
-plt.xscale('log')
-plt.savefig(dir+'Energy.png')
+plt.xlabel('time')
+plt.ylabel('Energy')
+plt.savefig('%sEnergyplot.png'%dir)
 plt.show()
 
